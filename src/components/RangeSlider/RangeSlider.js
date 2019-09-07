150aedd7e0fb4c64d11935151d6b981c00ce43b9
Christopher Geary <dev@crgeary.com>
2019-09-07 16:13:28 +0100

    add tooltips to each of the "rules" in the range slider

--- src/components/RangeSlider/RangeSlider.js
+++ src/components/RangeSlider/RangeSlider.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import Tooltip from 'react-tooltip-lite';
 
 import './RangeSlider.scss';
 
@@ -8,9 +9,19 @@ export default function RangeSlider ({ current, doRangeSliderChange, doSetRangeS
         <div className="range-slider">
             <div class="range-slider__control">
                 <span class="range-slider__current" style={{ left: `calc(${currentPercentage}% - ${(16/100)*currentPercentage}px - 12px)` }}>{current}</span>
-                <button class="range-slider__indicator range-slider__indicator--3:1" onClick={() => doSetRangeSlider(3)}>3:1</button>
-                <button class="range-slider__indicator range-slider__indicator--4.5:1" onClick={() => doSetRangeSlider(4.5)}>4.5:1</button>
-                <button class="range-slider__indicator range-slider__indicator--7:1" onClick={() => doSetRangeSlider(7)}>7:1</button>
+                
+                <button class="range-slider__indicator range-slider__indicator--3:1" onClick={() => doSetRangeSlider(3)}>
+                    <Tooltip arrowSize={6} distance={4} content="AA Large">3:1</Tooltip>
+                </button>
+                
+                <button class="range-slider__indicator range-slider__indicator--4.5:1" onClick={() => doSetRangeSlider(4.5)}>
+                    <Tooltip arrowSize={6} distance={4} content="AA Normal, AAA Large">4.5:1</Tooltip>
+                </button>
+
+                <button class="range-slider__indicator range-slider__indicator--7:1" onClick={() => doSetRangeSlider(7)}>
+                    <Tooltip arrowSize={6} distance={4} content="AAA Normal">7:1</Tooltip>
+                </button>
+
                 <input type="range" min="0" max="21" step=".01" value={current} onChange={doRangeSliderChange} />
             </div>
         </div>
